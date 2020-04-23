1e5bfa899284ed3de259a4e589538ec991c6c19b
shreyaSharma1708 <64209914+shreyaSharma1708@users.noreply.github.com>
2020-04-23 19:50:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# collisionAlgorithm
-designing the collision (isTouching) algorithm
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,30 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(600, 200, 20, 20);
+  movingRect.shapeColor = "green";
+  fixedRect = createSprite(400, 200, 20, 20);
+  fixedRect.shapeColor = "green";
+}
+
+function draw() {
+
+  background(0,0,0);
+  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(movingRect.x-fixedRect.x < movingRect.width/2 + fixedRect.width/2 &&   
+     fixedRect.x -movingRect.x< movingRect.width/2 + fixedRect.width/2 &&
+     movingRect.y-fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&   
+     fixedRect.y -movingRect.y< movingRect.height/2 + fixedRect.height/2 ){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }else{
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
